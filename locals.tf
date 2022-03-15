3223f98982e11784dfb2703ca92794aff5907d65
Igit <radengoots@users.noreply.github.com>
2022-03-15 10:00:05 +0700

    Deprecate s3 bucket logs target (#26)
    * limit aws provider version for compatiblity
    
    * add force destroy to s3 bucket, refactor cwl_logs_retention_days and change its default value
    
    * configure default expiration to the most aggresive setting

--- locals.tf
+++ locals.tf
@@ -3,8 +3,7 @@ locals {
   iam_policy_name   = "EC2PolicyToEnableSSM"
   ssm_document_name = "SSM-SessionManagerRunShell"
 
-  cwl_log_group_name      = "/aws/ssm/session-manager"
-  cwl_logs_retention_days = 90
+  cwl_log_group_name = "/aws/ssm/session-manager"
 
   s3_logging_bucket = "default-s3-logs-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
 }
